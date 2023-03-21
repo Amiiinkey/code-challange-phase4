e97f46616d8700bf7a07c7e0b4fc476c1bf23d59
Mohamed <aminito432@gmail.com>
2023-03-21 20:06:00 +0300

    added render_build and modified it's code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create]
+  # Route to handle not found errors
+  match "*path", to: "application#not_found", via: :all
 
   # Defines the root path route ("/")
   # root "articles#index"
